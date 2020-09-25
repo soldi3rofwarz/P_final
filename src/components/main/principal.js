2da946792698b974057982eeb3b0850191a757a1
Soldi3rofwarz <josejehovanicastroperez@gmail.com>
2020-09-25 13:57:29 -0600

    imagenes

--- src/components/main/principal.js
+++ src/components/main/principal.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import './main.css'
 
+
 const Index = () => {
+    const movimiento =()=>{
+        document.addEventListener("mousemove", function(e){
+            const bg = document.querySelector('.bg')
+            const bird =document.querySelector('.bird')
+            const content = document.querySelector('.content');
+            
+        
+        
+            bg.style.width = 100+e.pageX/100+ '%';
+            bg.style.height = 100+e.pageX/100+ '%';
+        
+            bird.style.right = 100+e.pageX/2+ 'px';
+            content.style.left = 100+e.pageX/2+ 'px';
+        })
+    }
     return ( 
-    <section className="main">
+    <section className="main"
+        onMouseMove={movimiento}
+    >
         <header>
         <a href="#" className="logo">world</a>
             <div className="toggle"></div>
         </header>
-        <img src="bg.jpg" alt=""  className="bg"/>
-        <img src="./../../assets/assetsMain/bird.png"  className="bird" alt="" />
+        <img src="./../../../assets/assetsMain/bg.jpg" className="bg"></img>
+        <img src="./../../../assets/assetsMain/bird.png"  className="bird" alt="" />
         <div className="content">
             <h2><br/><span>BagaMundo</span></h2>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
