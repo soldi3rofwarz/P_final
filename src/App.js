6e5d4775d9ca066b08a8767a3884a06fec4fb638
soldi3rofwarz <josejehovanicastroperez@gmail.com>
2020-10-06 15:03:09 -0600

    componiendo imagenes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import Principal from './../src/components/main/principal'
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from '@react-google-maps/api';
+//import{formatRelative} from 'date-fns'
 
+const libraries= ["places"]
+const mapContainerStyle={
+  width: "100vw",
+  height:"100vh"
+}
+const center = { lat: 12.105900, lng: -85.365060 }
 
 function App() {
+  const {isLoaded, loadError}= useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries,
+  })
+  if (loadError) return "Error alcargar el mapa"
+  if (!isLoaded) return "Cargando"
   return(
-    <Principal/>
+   <div>
+     <GoogleMap
+      mapContainerStyle={mapContainerStyle}
+      zoom={12} 
+      center={center}
+     />
+   </div>
   )
 }
 
--- src/components/main/principal.js
+++ src/components/main/principal.js
@@ -3,7 +3,8 @@ import './main.css'
 
 
 const Index = () => {
-    const movimiento =()=>{
+
+    const move = ()=>{
         document.addEventListener("mousemove", function(e){
             const bg = document.querySelector('.bg')
             const bird =document.querySelector('.bird')
@@ -17,30 +18,27 @@ const Index = () => {
             bird.style.right = 100+e.pageX/2+ 'px';
             content.style.left = 100+e.pageX/2+ 'px';
         })
+        
     }
     return ( 
     <section className="main"
-        onMouseMove={movimiento}
+        onMouseMove={move}
     >
         <header>
         <a href="#" className="logo">world</a>
             <div className="toggle"></div>
         </header>
-        <img src="./../../../assets/assetsMain/bg.jpg" className="bg"></img>
-        <img src="./../../../assets/assetsMain/bird.png"  className="bird" alt="" />
+        <img src={`${process.env.PUBLIC_URL}/res/bg.jpg`} alt=""  className="bg"/>
+        <img src={`${process.env.PUBLIC_URL}/res/bird.png`}  className="bird" alt="" />
         <div className="content">
             <h2><br/><span>BagaMundo</span></h2>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-                Distinctio alias, minima tempora doloremque neque molestias 
-                fugiat praesentium cum? Autem doloremque, officia esse nostrum 
-                delectus sequi quaerat inventore deleniti quam modi!
-            </p>
+           
             <a href="#">Unete</a>
         </div>
         <ul className="sci">
-            <li><a href="#"><img src="assets/facebook.png" alt="" /></a></li>
-            <li><a href="#"><img src="assets/instagram.png" alt="" /></a></li>
-            <li><a href="#"><img src="assets/twitter.png" alt="" /></a></li>
+            <li><a href="#"><img src={`${process.env.PUBLIC_URL}/res/facebook.png`} alt="" /></a></li>
+            <li><a href="#"><img src={`${process.env.PUBLIC_URL}/res/twitter.png`} alt="" /></a></li>
+            <li><a href="#"><img src={`${process.env.PUBLIC_URL}/res/instagram.png`} alt="" /></a></li>
         </ul>
 
     </section>
