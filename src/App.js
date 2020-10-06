97cb86eada9b9beb914239bc473f92f82672906b
soldi3rofwarz <josejehovanicastroperez@gmail.com>
2020-10-06 15:45:19 -0600

    agregando api mapa

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
   InfoWindow,
 } from '@react-google-maps/api';
 //import{formatRelative} from 'date-fns'
+import mapStyles from './components/mapa/mapStyles'
 
 const libraries= ["places"]
 const mapContainerStyle={
@@ -14,20 +15,27 @@ const mapContainerStyle={
   height:"100vh"
 }
 const center = { lat: 12.105900, lng: -85.365060 }
+const options = {
+  style: mapStyles,
+  disableDefaultUI: true,
+  zoomControl: true,
+}
 
 function App() {
   const {isLoaded, loadError}= useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    googleMapsApiKey: process.env.REACT_MAP_GOOGLE_MAPS_API_KEY= "AIzaSyBS7ep4NUFFXOgvazOhHuhMfIOysML2dos",
     libraries,
   })
   if (loadError) return "Error alcargar el mapa"
   if (!isLoaded) return "Cargando"
   return(
    <div>
+     <h1><span role="img" aria-label="tent">🏠</span></h1>
      <GoogleMap
       mapContainerStyle={mapContainerStyle}
       zoom={12} 
       center={center}
+      options={options}
      />
    </div>
   )
