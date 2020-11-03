d5a8f2fe2fc75f5fed71004fa503e89518b47da1
Soldi3rofwarz <josejehovanicastroperez@gmail.com>
2020-11-03 13:13:55 -0600

    s

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 //import Principal from './components/main/principal'
-//import Mapa from './components/mapa/mapa'
-import Detalle from './components/Detalles/Detalles'
+import Mapa from './components/mapa/mapa'
+//import Detalle from './components/Detalles/Detalles'
 //import Galeria from './components/gallery/galeria'
 // import Pie from './components/footer/foot'
 
@@ -10,7 +10,7 @@ function App() {
   return(
     
    <div>
-     <Detalle/>
+     <Mapa/>
      
    </div> 
    
