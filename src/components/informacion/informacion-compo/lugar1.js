05ddc3956c31da9b0f51cbf11ed45c9ea79c6f1e
Soldi3rofwarz <josejehovanicastroperez@gmail.com>
2021-01-03 22:45:42 -0600

    agregando react-photo-gallery

--- src/components/informacion/informacion-compo/lugar1.js
+++ src/components/informacion/informacion-compo/lugar1.js
@@ -1,32 +1,57 @@
 import React from 'react';
 
 import './informacion.css'
-import Header from './../../header/Head';
+import Gallery from 'react-photo-gallery'
 
-const Lugar1 = (props) => {
+
+const PhotosSet= [
+
+{
+	src: '/imgl1/122.jpg',
+	width: 4,
+	height: 3
+},
+
+{
+	src: './imgl1/ad.jpg',
+	width: 4,
+	height: 3
+},
+
+
+]
+
+
+const Lugar1 = () => {
+
     
     return (
-      
+      	<div>
             <div class="row">
                 <h2>El Nancital</h2>
                     <h4>
-                    El Nancital un paraíso natural frente a Acoyapa
+                    	El Nancital un paraíso natural frente a Acoyapa
                     </h4>
                     <p>
-                    La mayoría de sus islas son vírgenes y están deshabitadas. 
-                    Las familias que viven en las más grandes llegaron allí hace muchos años, procedentes de Chontales
+                    	La mayoría de sus islas son vírgenes y están deshabitadas. 
+                    	Las familias que viven en las más grandes llegaron allí hace muchos años, procedentes de Chontales
                     </p>
                     <p>
-                    El Archipiélago el Nancital, es un bello paraíso conformado por 27 islas con una gran diversidad de flora y fauna, que el visitante nacional y extranjero disfruta al recorrer el Lago Cocibolca en lancha.
+                    	El Archipiélago el Nancital, es un bello paraíso conformado por 27 islas con una gran diversidad de flora y fauna, que el visitante nacional y 			   	extranjero disfruta al recorrer el Lago Cocibolca en lancha.
                     </p>
                     <p>
-                    32 kilómetros hay que recorrer en vehículo desde el municipio de Acoyapa Chontales, para disfrutar de esta belleza localizada en el centro de Nicaragua.
+                    	32 kilómetros hay que recorrer en vehículo desde el municipio de Acoyapa Chontales, para disfrutar de esta belleza localizada en el centro de 				Nicaragua.
                     </p>
                     <p>
-                    Durante el recorrido en las aguas frescas y cristalinas del Gran Lago de Nicaragua, se puede apreciar de una imponente vista de los volcanes Concepción y Maderas.
+                    	Durante el recorrido en las aguas frescas y cristalinas del Gran Lago de Nicaragua, se puede apreciar de una imponente vista de los volcanes 				Concepción y Maderas.
                     </p>
-                </div>               
-  
+                </div>
+
+		<div>
+			<Gallery photos = {PhotosSet}/>
+		</div>
+		               
+  	</div>
     );
 };
-export default Lugar1;
+export default Lugar1
