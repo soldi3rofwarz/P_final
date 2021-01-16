d4bf0fd583bbd28577254efc4f9d673dfa85a5cd
soldi3rofwarz <josejehovanicastroperez@gmail.com>
2021-01-15 20:47:36 -0600

    pruebas

--- src/App.test.js
+++ src/App.test.js
@@ -6,63 +6,86 @@ import { shallow, mount} from 'enzyme';
 import{ ActividadesPage } from './pages/actividades'
 import Det_page from './pages/detallePage'
 import LoginPage from './pages/login'
+import Lugar1 from './components/informacion/informacion-compo/lugar1'
+import Galeria from './components/gallery/galeria'
+import Header from './elements/theme/components/header'
 
 
-
-// test('renders learn react link', () => {
+//test('renders learn react link', () => {
 //     const { getByText } = render(<App />);
 //     const linkElement = getByText(/learn react/i);
 //     expect(linkElement).toBeInTheDocument();
 // });
 
-// const value=0
-// const setValue= value
+/*const value=0
+const setValue= value
 
-// const Limite=()=>{
-//   setValue(value + 1)
+const Limite=()=>{
+ setValue(value + 1)
 
-//   if(value === 20){
-//       setValue(value===0)
+ if(value === 20){
+      setValue(value===0)
       
-//   }
-// }
+ }
+}
 
-// describe('Actividades',()=>{
-//   it('Levantar  actividades sin errores', () => {
-//      shallow(<ActividadesPage/>);
+describe('prueba limites',()=>{
+  it('001. probar limite de participantes', ()=>{
+    const wrapper= shallow(<Det_page onClick={Limite}/>);
+
+  })
+})*/
+
+/*describe('Detalles',()=>{
+  it('002. provar existencia de mapa', () => {
+    const wrapper= shallow(<Det_page/>);
+    const mapa = wrapper.find('mapa')
+    expect(mapa) 
+  });
+})*/
+
+/*describe('Actividades',()=>{
+  it('003. Levantar  actividades sin errores', () => {
+     shallow(<ActividadesPage/>);
      
-//   });
-// })
+  });
+})*/
 
-// describe('Detalles',()=>{
-//   it('provar existencia de mapa', () => {
-//     const wrapper= shallow(<Det_page/>);
-//     const mapa = wrapper.find('mapa')
-//     expect(mapa) 
-//   });
-// })
-
-// describe('prueba limites',()=>{
-//   it('probar limite de participantes', ()=>{
-//     const wrapper= shallow(<Det_page onClick={Limite}/>);
-
-//   })
-// })
-
-
-describe('vista de login ',()=>{
-  it('Levantar vista sin errores', () => {
+/*describe('Actividades',()=>{
+  it('004. Levantar  login sin errores', () => {
      shallow(<LoginPage/>);
      
   });
-})
+})*/
 
-describe('campo contraseña ',()=>{
-  it('verificar si la contraseña esta oculta', () => {
+/*describe('campo contraseña ',()=>{
+  it('005. verificar si la contraseña esta oculta', () => {
     const wrapper= shallow(<LoginPage/>);
     const pass = wrapper.find("password")
     expect(pass.type== "password")
      
   });
-})
+})*/
 
+
+/*describe('Informacion de galeria',()=>{
+  it('006. verificar si el titulo  de la vista es igual al titulo solicitado', () => {
+    const wrapper= shallow(<Lugar1/>);
+    const h2 = wrapper.find('h2')
+    expect(h2.text()).toEqual("El Nancital")
+     
+  });
+})*/
+
+/*describe('Galeria de imagenes ',()=>{
+  it('007. Levantar vista sin errores', () => {
+     shallow(<Galeria/>);
+  });
+})*/
+
+/*describe('Navbar',()=>{
+  it('008. probar si existe enrutamiento con el componente Link', () => {
+     const Wrapper = shallow(<Header/>);
+
+  });
+})*/
